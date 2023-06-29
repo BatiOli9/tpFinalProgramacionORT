891580fcaf5fa0a5b506bb9b5aa224e98d552cef
BatiOli9 <oliversafdieh@gmail.com>
2023-06-29 12:07:06 -0300

    Agregar archivos de proyecto.

---
+++ tpFinalProgramacion/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace tpFinalProgramacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tbMinuto = new System.Windows.Forms.TextBox();
+            this.lblMinuto = new System.Windows.Forms.Label();
+            this.lblEquipoContra = new System.Windows.Forms.Label();
+            this.tbEquipoContra = new System.Windows.Forms.TextBox();
+            this.lblArqueroContra = new System.Windows.Forms.Label();
+            this.tbArqueroContra = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft JhengHei", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(29, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(740, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Buenos Dias! Bienvenido al programa para contar los goles de Messi";
+            // 
+            // tbMinuto
+            // 
+            this.tbMinuto.Location = new System.Drawing.Point(12, 97);
+            this.tbMinuto.Name = "tbMinuto";
+            this.tbMinuto.Size = new System.Drawing.Size(236, 20);
+            this.tbMinuto.TabIndex = 2;
+            // 
+            // lblMinuto
+            // 
+            this.lblMinuto.AutoSize = true;
+            this.lblMinuto.BackColor = System.Drawing.Color.Transparent;
+            this.lblMinuto.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinuto.ForeColor = System.Drawing.Color.White;
+            this.lblMinuto.Location = new System.Drawing.Point(12, 76);
+            this.lblMinuto.Name = "lblMinuto";
+            this.lblMinuto.Size = new System.Drawing.Size(196, 20);
+            this.lblMinuto.TabIndex = 3;
+            this.lblMinuto.Text = "Ingresa el minuto del gol";
+            // 
+            // lblEquipoContra
+            // 
+            this.lblEquipoContra.AutoSize = true;
+            this.lblEquipoContra.BackColor = System.Drawing.Color.Transparent;
+            this.lblEquipoContra.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipoContra.ForeColor = System.Drawing.Color.White;
+            this.lblEquipoContra.Location = new System.Drawing.Point(8, 136);
+            this.lblEquipoContra.Name = "lblEquipoContra";
+            this.lblEquipoContra.Size = new System.Drawing.Size(208, 20);
+            this.lblEquipoContra.TabIndex = 4;
+            this.lblEquipoContra.Text = "Ingresa contra que equipo";
+            // 
+            // tbEquipoContra
+            // 
+            this.tbEquipoContra.Location = new System.Drawing.Point(12, 159);
+            this.tbEquipoContra.Name = "tbEquipoContra";
+            this.tbEquipoContra.Size = new System.Drawing.Size(236, 20);
+            this.tbEquipoContra.TabIndex = 5;
+            // 
+            // lblArqueroContra
+            // 
+            this.lblArqueroContra.AutoSize = true;
+            this.lblArqueroContra.BackColor = System.Drawing.Color.Transparent;
+            this.lblArqueroContra.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArqueroContra.ForeColor = System.Drawing.Color.White;
+            this.lblArqueroContra.Location = new System.Drawing.Point(8, 207);
+            this.lblArqueroContra.Name = "lblArqueroContra";
+            this.lblArqueroContra.Size = new System.Drawing.Size(215, 20);
+            this.lblArqueroContra.TabIndex = 6;
+            this.lblArqueroContra.Text = "Ingresa contra que arquero";
+            // 
+            // tbArqueroContra
+            // 
+            this.tbArqueroContra.Location = new System.Drawing.Point(12, 231);
+            this.tbArqueroContra.Name = "tbArqueroContra";
+            this.tbArqueroContra.Size = new System.Drawing.Size(236, 20);
+            this.tbArqueroContra.TabIndex = 7;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(16, 308);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 22);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "PSG";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Selecciona en que equipo estaba jugando";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(98, 308);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(94, 22);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Barcelona";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.ForeColor = System.Drawing.Color.White;
+            this.radioButton3.Location = new System.Drawing.Point(206, 308);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(162, 22);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Seleccion Argentina";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 332);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 45);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(102, 332);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(98, 45);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(236, 331);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(98, 45);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(63, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Agregar Gol";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.tbArqueroContra);
+            this.Controls.Add(this.lblArqueroContra);
+            this.Controls.Add(this.tbEquipoContra);
+            this.Controls.Add(this.lblEquipoContra);
+            this.Controls.Add(this.lblMinuto);
+            this.Controls.Add(this.tbMinuto);
+            this.Controls.Add(this.lblTitle);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox tbMinuto;
+        private System.Windows.Forms.Label lblMinuto;
+        private System.Windows.Forms.Label lblEquipoContra;
+        private System.Windows.Forms.TextBox tbEquipoContra;
+        private System.Windows.Forms.Label lblArqueroContra;
+        private System.Windows.Forms.TextBox tbArqueroContra;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
